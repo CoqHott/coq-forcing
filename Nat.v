4dad82fb1ef916c9597446bc2be3415c2c0a71cc
nicolas.tabareau <nicolas.tabareau@inria.fr>
2015-12-17 12:40:32 +0100

    nat with dependent elimination
    Not very nice, but it works

--- Nat.v
+++ Nat.v
@@ -22,37 +22,38 @@ Defined.
 
 Forcing Definition O : nat using Obj Hom.
 Proof.
-intros.
-apply O_.
+  exact O_. 
 Defined.
 
 Forcing Definition S : nat -> nat using Obj Hom.
 Proof.
-intros p n.
-apply S_.
-apply n.
+  exact S_. 
 Defined.
 
-Forcing Definition nat_rec : forall (P : Type), P -> (P -> P) -> nat -> P using Obj Hom.
-Proof.
-  intros p P H0 HS n. compute in *.
-
-  exact ((fix F (p' : Obj)
-              (P :  forall p0 : Obj, p' ≤ p0 -> forall p : Obj, p0 ≤ p -> Type)
-              (H0 : forall (p0 : Obj) (α : p' ≤ p0), P p0 α p0 #)
-              (HS:  forall (p0 : Obj) (α : p' ≤ p0),
-                    (forall (p : Obj) (α0 : p0 ≤ p), P p (α ∘ α0) p #) -> P p0 α p0 #)
-              (n : nat_ p') {struct n} : P p' # p' # :=
-            match n with
-            | O_ _ =>    H0 p' #
-            | S_ _ n0 => HS p' #
-                   (fun (p1 : Obj) (α1 : p' ≤ p1) =>
-                      F p1
+Fixpoint nat_rec_ (p : Obj)
+  (P : forall p0 : Obj, p ≤ p0 -> forall p : Obj, p0 ≤ p -> Type)
+  (H0 : forall (p0 : Obj) (α : p ≤ p0),
+       P p0 (fun (R : Obj) (k : Hom p0 R) => α R k) p0 #)
+  (HS : forall (p0 : Obj) (α : p ≤ p0),
+       (forall (p : Obj) (α0 : p0 ≤ p), P p (α ∘ α0) p #) ->
+       P p0 (fun (R : Obj) (k : Hom p0 R) => α R k) p0 #)
+  (n : nat_ p):
+   P p # p # := match n with
+            | O_ _ =>    H0 p #
+            | S_ _ n0 => HS p #
+                   (fun (p1 : Obj) (α1 : p ≤ p1) =>
+                      nat_rec_ p1
                         (fun p1 f1 => P  p1 (α1 ∘ f1))
                         (fun p1 f1 => H0 p1 (α1 ∘ f1))
                         (fun p1 f1 => HS p1 (α1 ∘ f1))
                         (n0 p1 α1))
-            end) p P H0 HS (n p #)).
+            end.
+
+
+Forcing Definition nat_rec : forall (P : Type), P -> (P -> P) -> nat -> P using Obj Hom.
+Proof.
+  intros p P H0 HS n. 
+  exact (nat_rec_ p P H0 HS (n p #)).
 Defined.
 
 Definition foo := fun (P : Type) (H0 : P) (HS : P -> P) => nat_rec P H0 HS O.
@@ -67,5 +68,82 @@ Eval compute in ᶠbar.
 Eval compute in ᶠqux.
 
 Check (eq_refl : ᶠbar = ᶠqux).
+
+Definition nat_mem : forall R, nat -> (nat -> R) -> R :=
+  fun R : Type =>
+    nat_rec ((nat -> R) -> R) (fun f => f O)
+            (fun H f => H (fun n => f (S n))).
+
+Forcing Translate nat_mem using Obj Hom.
+
+Forcing Definition nat_rect : forall (P : nat -> Type),
+    P O ->
+    (forall (n:nat), nat_mem _ n P -> nat_mem _ (S n) P) ->
+    forall n : nat, nat_mem _ n P using Obj Hom.
+Proof.
+  intros p P H0 HS n. unfold ᶠnat_mem, ᶠnat_rec . set (n0 := n p #). clearbody n0; clear n.
+  exact ((fix F p 
+              (P : forall p0 : Obj,
+                  p ≤ p0 ->
+                  (forall p : Obj, p0 ≤ p -> ᶠnat p p #) ->
+                  forall p : Obj, p0 ≤ p -> Type)
+              (H0 : forall (p0 : Obj) (α : p ≤ p0),
+       P p0 (# ∘ (α ∘ #)) (fun (p : Obj) (_ : p0 ≤ p) => ᶠO p) p0 #)
+              (HS :forall (p0 : Obj) (α : p ≤ p0)
+         (n : forall p : Obj, p0 ≤ p -> ᶠnat p p #),
+       (forall (p1 : Obj) (α0 : p0 ≤ p1),
+        ᶠnat_mem p1
+          (fun (p : Obj) (_ : p1 ≤ p) (p2 : Obj) (_ : p ≤ p2) =>
+           forall p3 : Obj, p2 ≤ p3 -> Type)
+          (fun (p : Obj) (α1 : p1 ≤ p) => n p (# ∘ (α0 ∘ (α1 ∘ #))))
+          (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+           P p2 (# ∘ (# ∘ (α ∘ (# ∘ (# ∘ (α0 ∘ (α1 ∘ #)))))))) p1 
+          #) ->
+       ᶠnat_mem p0
+         (fun (p : Obj) (_ : p0 ≤ p) (p1 : Obj) (_ : p ≤ p1) =>
+          forall p2 : Obj, p1 ≤ p2 -> Type)
+         (fun (p : Obj) (α0 : p0 ≤ p) =>
+          ᶠS p
+            (fun (p1 : Obj) (α1 : p ≤ p1) => n p1 (# ∘ (α0 ∘ (α1 ∘ #)))))
+         (fun (p1 : Obj) (α0 : p0 ≤ p1) =>
+          P p1 (α ∘ α0)) p0 
+         #)
+              (n0 : ᶠnat p p #) : nat_rec_ p
+     (fun (p0 : Obj) (_ : p ≤ p0) (p1 : Obj) (_ : p0 ≤ p1) =>
+      (forall p2 : Obj,
+       p1 ≤ p2 ->
+       (forall p3 : Obj, p2 ≤ p3 -> ᶠnat p3 p3 #) ->
+       forall p3 : Obj, p2 ≤ p3 -> Type) ->
+      forall p2 : Obj, p1 ≤ p2 -> Type) _ _ n0
+     (fun (p0 : Obj) (α : p ≤ p0) =>
+      P p0 (fun (R : Obj) (k : Hom p0 R) => α R k)) p #
+             := match n0 with
+            | O_ _ =>   H0 p #
+            | S_ _ n => HS p # n
+                   (fun (p1 : Obj) (α1 : p ≤ p1) =>
+                      F p1
+                        (fun p1 f1 => P  p1 (α1 ∘ f1))
+                        (fun p1 f1 => H0 p1 (α1 ∘ f1))
+                        (fun p1 f1 => HS p1 (α1 ∘ f1))
+                        (n p1 α1)) end
+          ) p P H0 HS n0).
+Defined.
+
+
+Definition bar2 := fun (P : nat -> Type) (H0 : P O)
+    (HS : (forall (n:nat), nat_mem _ n P -> nat_mem _ (S n) P))
+    (n : nat) => nat_rect P H0 HS (S n).
+Definition qux2 := fun (P : nat -> Type) (H0 : P O)
+    (HS : (forall (n:nat), nat_mem _ n P -> nat_mem _ (S n) P))
+    (n : nat) => HS n (nat_rect P H0 HS n).
+
+Forcing Translate bar2 using Obj Hom.
+Forcing Translate qux2 using Obj Hom.
+
+Eval compute in ᶠbar2.
+Eval compute in ᶠqux2.
+
+Check (eq_refl : ᶠbar2 = ᶠqux2).
+
 
 End Nat.
