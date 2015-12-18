2b2263f427902bb86a676d4bfdeb1b0cee2cc452
nicolas.tabareau <nicolas.tabareau@inria.fr>
2015-12-18 10:08:20 +0100

    lists with dependent elimination

--- List.v
+++ List.v
@@ -11,142 +11,250 @@ Notation "f ∘ g" := (fun (R : Obj) (k : Hom _ R) => f R (g R k)) (at level 40)
 
 
 Inductive list_ (p : Obj) (A : forall q (f : p ≤ q), Type) : Type :=
-| nil_ : list_ p A
-| cons_ : (forall q f, A q f) -> (forall q (f : p ≤ q), list_ q (fun r g => A r (f ∘ g))) -> list_ p A.
+| nil_  : list_ p A
+| cons_ : (forall q f, A q f) ->
+          (forall q (f : p ≤ q), list_ q (fun r g => A r (f ∘ g))) ->
+          list_ p A.
 
 Forcing Definition list : Type -> Type using Obj Hom.
 Proof.
-intros p A q f.
-exact (list_ q (fun r g => A r (f ∘ g) r #)).
+  intros p A q f.
+  exact (list_ q (fun r g => A r (f ∘ g) r #)).
 Defined.
-
 
 Forcing Definition nil : forall A, list A using Obj Hom.
 Proof.
-intros.
-apply nil_.
+  intros p A.
+  exact (nil_ p (fun r g => A r g r #)). 
 Defined.
 
 Forcing Definition cons : forall A, A -> list A -> list A using Obj Hom.
 Proof.
-intros p A x l.
-apply cons_.
-+ intros r g. apply x.
-+ intros r g. apply l.
+  intros p A x l.
+  exact (cons_ p (fun r g => A r g r #) x l). 
 Defined.
 
-Definition list_rec_ := 
-(fun (p : Obj) (A P : forall p0 : Obj, p ≤ p0 -> forall p1 : Obj, p0 ≤ p1 -> Type)
-   (H0 : forall (p0 : Obj) (α : p ≤ p0), P p0 (# ∘ (α ∘ #)) p0 #)
-   (HS : forall (p0 : Obj) (α : p ≤ p0),
-         (forall (p1 : Obj) (α0 : p0 ≤ p1), A p1 (# ∘ (# ∘ (# ∘ (α ∘ (# ∘ (α0 ∘ #)))))) p1 #) ->
-         (forall (p1 : Obj) (α0 : p0 ≤ p1), P p1 (# ∘ (# ∘ (α ∘ (# ∘ (# ∘ (α0 ∘ #)))))) p1 #) ->
-         P p0 (# ∘ (# ∘ (α ∘ (# ∘ (# ∘ #))))) p0 #)
-   (n : forall (p0 : Obj) (α : p ≤ p0),
-        ᶠlist p0 (fun (p1 : Obj) (α0 : p0 ≤ p1) => A p1 (# ∘ (# ∘ (# ∘ (# ∘ (α ∘ (α0 ∘ #))))))) p0 #) =>
- (fun H : forall (q : Obj) (f : p ≤ q), P q f q # => (fun X : forall (q : Obj) (f : p ≤ q), P q f q # => X p #) H)
-   (fun (q : Obj) (f : p ≤ q) =>
-    (fun n0 : list_ q (fun (r : Obj) (g : q ≤ r) => A r (f ∘ g) r #) =>
-     let A0 := fun (r : Obj) (g : q ≤ r) => A r (f ∘ g) r # in
-     let HeqA0 := eq_refl in
-     list__rect
-       (fun (q0 : Obj) (A1 : forall q1 : Obj, q0 ≤ q1 -> Type) (_ : list_ q0 A1) =>
-        forall f0 : p ≤ q0, A1 = (fun (r : Obj) (g : q0 ≤ r) => A r (f0 ∘ g) r #) -> P q0 f0 q0 #)
-       (fun (p0 : Obj) (A1 : forall q0 : Obj, p0 ≤ q0 -> Type) (f0 : p ≤ p0)
-          (_ : A1 = (fun (r : Obj) (g : p0 ≤ r) => A r (f0 ∘ g) r #)) => H0 p0 f0)
-       (fun (p0 : Obj) (A1 : forall q0 : Obj, p0 ≤ q0 -> Type) (a : forall (q0 : Obj) (f0 : p0 ≤ q0), A1 q0 f0)
-          (l : forall (q0 : Obj) (f0 : p0 ≤ q0), list_ q0 (fun (r : Obj) (g : q0 ≤ r) => A1 r (f0 ∘ g)))
-          (X : forall (q0 : Obj) (f0 : p0 ≤ q0) (f1 : p ≤ q0),
-               (fun (r : Obj) (g : q0 ≤ r) => A1 r (f0 ∘ g)) = (fun (r : Obj) (g : q0 ≤ r) => A r (f1 ∘ g) r #) ->
-               P q0 f1 q0 #) (f0 : p ≤ p0) (HeqA1 : A1 = (fun (r : Obj) (g : p0 ≤ r) => A r (f0 ∘ g) r #)) =>
-        HS p0 f0
-          (fun (p1 : Obj) (α : p0 ≤ p1) =>
-           (fun HeqA2 : (fun (r : Obj) (g : p0 ≤ r) => A r (f0 ∘ g) r #) = A1 =>
-            match
-              HeqA2 in (_ = y)
-              return
-                ((forall (q0 : Obj) (f1 : p0 ≤ q0), y q0 f1) ->
-                 (forall (q0 : Obj) (f1 : p0 ≤ q0), list_ q0 (fun (r : Obj) (g : q0 ≤ r) => y r (f1 ∘ g))) ->
-                 (forall (q0 : Obj) (f1 : p0 ≤ q0) (f2 : p ≤ q0),
-                  (fun (r : Obj) (g : q0 ≤ r) => y r (f1 ∘ g)) = (fun (r : Obj) (g : q0 ≤ r) => A r (f2 ∘ g) r #) ->
-                  P q0 f2 q0 #) -> A p1 (f0 ∘ α) p1 #)
-            with
-            | eq_refl =>
-                fun (a0 : forall (q0 : Obj) (f1 : p0 ≤ q0), A q0 (f0 ∘ f1) q0 #)
-                  (_ : forall (q0 : Obj) (f1 : p0 ≤ q0),
-                       list_ q0
-                         (fun (r : Obj) (g : q0 ≤ r) => A r (fun (R : Obj) (k : Hom r R) => f0 R (f1 R (g R k))) r #))
-                  (_ : forall (q0 : Obj) (f1 : p0 ≤ q0) (f2 : p ≤ q0),
-                       (fun (r : Obj) (g : q0 ≤ r) => A r (fun (R : Obj) (k : Hom r R) => f0 R (f1 R (g R k))) r #) =
-                       (fun (r : Obj) (g : q0 ≤ r) => A r (f2 ∘ g) r #) -> P q0 f2 q0 #) => 
-                a0 p1 α
-            end a l X) (eq_sym HeqA1))
-          (fun (r : Obj) (g : p0 ≤ r) =>
-           (fun HeqA2 : (fun (r0 : Obj) (g0 : p0 ≤ r0) => A r0 (f0 ∘ g0) r0 #) = A1 =>
-            match
-              HeqA2 in (_ = y)
-              return
-                ((forall (q0 : Obj) (f1 : p0 ≤ q0), y q0 f1) ->
-                 (forall (q0 : Obj) (f1 : p0 ≤ q0), list_ q0 (fun (r0 : Obj) (g0 : q0 ≤ r0) => y r0 (f1 ∘ g0))) ->
-                 (forall (q0 : Obj) (f1 : p0 ≤ q0) (f2 : p ≤ q0),
-                  (fun (r0 : Obj) (g0 : q0 ≤ r0) => y r0 (f1 ∘ g0)) =
-                  (fun (r0 : Obj) (g0 : q0 ≤ r0) => A r0 (f2 ∘ g0) r0 #) -> P q0 f2 q0 #) -> 
-                 P r (f0 ∘ g) r #)
-            with
-            | eq_refl =>
-                fun (_ : forall (q0 : Obj) (f1 : p0 ≤ q0), A q0 (f0 ∘ f1) q0 #)
-                  (_ : forall (q0 : Obj) (f1 : p0 ≤ q0),
-                       list_ q0
-                         (fun (r0 : Obj) (g0 : q0 ≤ r0) =>
-                          A r0 (fun (R : Obj) (k : Hom r0 R) => f0 R (f1 R (g0 R k))) r0 #))
-                  (X0 : forall (q0 : Obj) (f1 : p0 ≤ q0) (f2 : p ≤ q0),
-                        (fun (r0 : Obj) (g0 : q0 ≤ r0) =>
-                         A r0 (fun (R : Obj) (k : Hom r0 R) => f0 R (f1 R (g0 R k))) r0 #) =
-                        (fun (r0 : Obj) (g0 : q0 ≤ r0) => A r0 (f2 ∘ g0) r0 #) -> P q0 f2 q0 #) =>
-                X0 r g (f0 ∘ g) eq_refl
-            end a l X) (eq_sym HeqA1))) q A0 n0 f HeqA0) (n q f)))
-.
+Fixpoint list_rec_ p
+         (A P : forall p0 : Obj, p ≤ p0 -> forall p1 : Obj, p0 ≤ p1 -> Type)
+         (Hnil : forall (p0 : Obj) (α : p ≤ p0), P p0 α p0 #)
+         (Hcons: forall (p0 : Obj) (α : p ≤ p0),
+             (forall (p : Obj) (α0 : p0 ≤ p), A p (α ∘ α0) p #) ->
+             (forall (p1 : Obj) (α0 : p0 ≤ p1),
+                 ᶠlist p1 (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+                             A p2 (α ∘ (α0 ∘ α1))) p1 #) ->
+             (forall (p : Obj) (α0 : p0 ≤ p), P p (α ∘ α0) p #) ->
+             P p0 α p0 #)
+         (l : ᶠlist p A p #) {struct l} : P p # p # :=
+            match l with
+            | nil_ _ _ =>       Hnil p #
+            | cons_ _ _ ny ll => Hcons p # ny ll
+                   (fun (p1 : Obj) (α1 : p ≤ p1) =>
+                      list_rec_ p1 
+                        (fun p1 f1 => A     p1 (α1 ∘ f1))
+                        (fun p1 f1 => P     p1 (α1 ∘ f1))
+                        (fun p1 f1 => Hnil  p1 (α1 ∘ f1))
+                        (fun p1 f1 => Hcons p1 (α1 ∘ f1))
+                        (ll p1 α1))
+            end.
 
-Forcing Definition list_rec : forall A (P : Type), P -> (A -> P -> P) -> list A -> P using Obj Hom.
+Forcing Definition list_rec : forall A (P : Type), P -> (A -> list A -> P -> P) -> list A -> P using Obj Hom.
 Proof.
-exact list_rec_.
+  intros p A P Hnil Hcons l.
+  exact (list_rec_ p A P Hnil Hcons (l p #)).
 Defined.
 
-Definition foo := fun A (P : Type) (H0 : P) (HS : A -> P -> P) => list_rec A P H0 HS (nil A).
-Definition bar := fun A (P : Type) (H0 : P) (HS : A -> P -> P) (x : A) (l : list A) => list_rec A P H0 HS (cons A x l).
-Definition qux := fun A (P : Type) (H0 : P) (HS : A -> P -> P) (x : A) (l : list A) => HS x (list_rec A P H0 HS l).
+Definition bar := fun A (P : Type) (H0 : P) (HS : A -> list A -> P -> P) (x : A) (l : list A) => list_rec A P H0 HS (cons A x l).
 
-Forcing Translate foo using Obj Hom.
+Definition qux := fun A (P : Type) (H0 : P) (HS : A -> list A -> P -> P) (x : A) (l : list A) => HS x l (list_rec A P H0 HS l).
+
 Forcing Translate bar using Obj Hom.
 Forcing Translate qux using Obj Hom.
 
 Eval compute in ᶠbar.
 Eval compute in ᶠqux.
 
-Fail Check (eq_refl : ᶠbar = ᶠqux).
+Check (eq_refl : ᶠbar = ᶠqux).
 
-Forcing Definition list_rec' : forall A (P : Type), P -> (A -> P -> P) -> list A -> P using Obj Hom.
+Definition list_mem : forall A R, list A -> (list A -> R) -> R :=
+  fun A R : Type =>
+    list_rec A ((list A -> R) -> R) (fun f => f (nil A))
+             (fun a _ H f => H (fun ll => f (cons A a ll))).
+
+Forcing Translate list_mem using Obj Hom.
+
+Forcing Definition list_rect : forall A (P : list A -> Type),
+    P (nil _) ->
+    (forall (a:A) (l:list A), list_mem _ _ l P -> list_mem _ _ (cons _ a l) P) ->
+    forall l : list A, list_mem _ _ l P using Obj Hom.
 Proof.
-intros p A P H0 HS n.
-compute in *.
-cut (forall q f, P q f q #).
-{ intros X. apply X. }
+  intros p A P Hnil Hcons l.
 
-intros q f.
-specialize (n q f).
-remember (fun (r : Obj) (g : q ≤ r) => A r (f ∘ g) r #) as A0.
-induction n.
+  (* avoiding noise in the actual definition *)
+  (* may be improved using LTac ? *)
+  
+  pose (Type_of_A := fun p => forall p0 : Obj, p ≤ p0 -> forall p : Obj, p0 ≤ p -> Type).
+  pose (Type_of_P := fun p (A : Type_of_A p) => forall (p0 : Obj) (α : p ≤ p0),
+                  (forall (p : Obj) (α0 : p0 ≤ p),
+                      list_ p
+                            (fun (r : Obj) (g : p ≤ r) =>
+                               A r (fun (R : Obj) (k : Hom r R) => α R (α0 R (g R k))) r #)) ->
+                  forall p : Obj, p0 ≤ p -> Type).
+  pose (Type_of_Hnil := fun p (A : Type_of_A p) (P : Type_of_P p A) =>
+                          forall (p0 : Obj) (α : p ≤ p0),
+         P p0 (fun (R : Obj) (k : Hom p0 R) => α R k)
+           (fun (p : Obj) (α0 : p0 ≤ p) =>
+            ᶠnil p
+              (fun (p1 : Obj) (α1 : p ≤ p1) =>
+               A p1 (fun (R : Obj) (k : Hom p1 R) => α R (α0 R (α1 R k)))))
+           p0 #).
+  pose (Type_of_Hcons := fun p (A : Type_of_A p) (P : Type_of_P p A) =>
+                           forall (p0 : Obj) (α : p ≤ p0)
+            (a : forall (p : Obj) (α0 : p0 ≤ p), A p (α ∘ α0) p #)
+            (l : forall (p : Obj) (α0 : p0 ≤ p),
+                 ᶠlist p
+                   (fun (p1 : Obj) (α1 : p ≤ p1) =>
+                    A p1
+                      (fun (R : Obj) (k : Hom p1 R) => α R (α0 R (α1 R k))))
+                   p #),
+          (forall (p : Obj) (α0 : p0 ≤ p),
+           ᶠlist_mem p
+             (fun (p1 : Obj) (α1 : p ≤ p1) =>
+              A p1 (fun (R : Obj) (k : Hom p1 R) => α R (α0 R (α1 R k))))
+             (fun (p1 : Obj) (_ : p ≤ p1) (p2 : Obj) (_ : p1 ≤ p2) =>
+              forall p3 : Obj, p2 ≤ p3 -> Type)
+             (fun (p1 : Obj) (α1 : p ≤ p1) => l p1 (α0 ∘ α1))
+             (fun (p1 : Obj) (α1 : p ≤ p1) =>
+              P p1 (fun (R : Obj) (k : Hom p1 R) => α R (α0 R (α1 R k))))
+             p #) ->
+          ᶠlist_mem p0 (fun (p : Obj) (α0 : p0 ≤ p) => A p (α ∘ α0))
+            (fun (p : Obj) (_ : p0 ≤ p) (p1 : Obj) (_ : p ≤ p1) =>
+             forall p2 : Obj, p1 ≤ p2 -> Type)
+            (fun (p : Obj) (α0 : p0 ≤ p) =>
+             ᶠcons p
+               (fun (p1 : Obj) (α1 : p ≤ p1) =>
+                A p1 (fun (R : Obj) (k : Hom p1 R) => α R (α0 R (α1 R k))))
+               (fun (p1 : Obj) (α1 : p ≤ p1) => a p1 (α0 ∘ α1))
+               (fun (p1 : Obj) (α1 : p ≤ p1) => l p1 (α0 ∘ α1)))
+            (fun (p : Obj) (α0 : p0 ≤ p) => P p (α ∘ α0)) p0 
+            #).
+  pose (Goal_Type := fun  p (A : Type_of_A p) (P : Type_of_P p A)
+              (Hnil : Type_of_Hnil p A P)
+              (Hcons : Type_of_Hcons p A P)
+              (l0 : ᶠlist p A p #) =>
+ list_rec_ p
+     (fun (p0 : Obj) (α : p ≤ p0) =>
+      A p0 (fun (R : Obj) (k : Hom p0 R) => α R k))
+     (fun (p0 : Obj) (α : p ≤ p0) (p1 : Obj) (α0 : p0 ≤ p1) =>
+      (forall (p2 : Obj) (α1 : p1 ≤ p2),
+       (forall (p3 : Obj) (α2 : p2 ≤ p3),
+        ᶠlist p3
+          (fun (p4 : Obj) (α3 : p3 ≤ p4) =>
+           A p4
+             (fun (R : Obj) (k : Hom p4 R) =>
+              α R (α0 R (α1 R (α2 R (α3 R k)))))) p3 
+          #) -> forall p3 : Obj, p2 ≤ p3 -> Type) ->
+      forall p2 : Obj, p1 ≤ p2 -> Type)
+     (fun (p0 : Obj) (α : p ≤ p0)
+        (f : forall (p1 : Obj) (α0 : p0 ≤ p1),
+             (forall (p2 : Obj) (α1 : p1 ≤ p2),
+              ᶠlist p2
+                (fun (p3 : Obj) (α2 : p2 ≤ p3) =>
+                 A p3
+                   (fun (R : Obj) (k : Hom p3 R) =>
+                    α R (α0 R (α1 R (α2 R k))))) p2 
+                #) -> forall p2 : Obj, p1 ≤ p2 -> Type) =>
+      f p0 #
+        (fun (p1 : Obj) (α0 : p0 ≤ p1) =>
+         ᶠnil p1
+           (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+            A p2 (fun (R : Obj) (k : Hom p2 R) => α R (α0 R (α1 R k))))))
+     (fun (p0 : Obj) (α : p ≤ p0)
+        (a : forall (p1 : Obj) (α0 : p0 ≤ p1), A p1 (α ∘ α0) p1 #)
+        (_ : forall (p1 : Obj) (α0 : p0 ≤ p1),
+             ᶠlist p1
+               (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+                A p2 (fun (R : Obj) (k : Hom p2 R) => α R (α0 R (α1 R k))))
+               p1 #)
+        (H : forall (p1 : Obj) (α0 : p0 ≤ p1),
+             (forall (p2 : Obj) (α1 : p1 ≤ p2),
+              (forall (p3 : Obj) (α2 : p2 ≤ p3),
+               ᶠlist p3
+                 (fun (p4 : Obj) (α3 : p3 ≤ p4) =>
+                  A p4
+                    (fun (R : Obj) (k : Hom p4 R) =>
+                     α R (α0 R (α1 R (α2 R (α3 R k)))))) p3 
+                 #) -> forall p3 : Obj, p2 ≤ p3 -> Type) ->
+             forall p2 : Obj, p1 ≤ p2 -> Type)
+        (f : forall (p1 : Obj) (α0 : p0 ≤ p1),
+             (forall (p2 : Obj) (α1 : p1 ≤ p2),
+              ᶠlist p2
+                (fun (p3 : Obj) (α2 : p2 ≤ p3) =>
+                 A p3
+                   (fun (R : Obj) (k : Hom p3 R) =>
+                    α R (α0 R (α1 R (α2 R k))))) p2 
+                #) -> forall p2 : Obj, p1 ≤ p2 -> Type) =>
+      H p0 #
+        (fun (p1 : Obj) (α0 : p0 ≤ p1)
+           (ll : forall (p2 : Obj) (α1 : p1 ≤ p2),
+                 ᶠlist p2
+                   (fun (p3 : Obj) (α2 : p2 ≤ p3) =>
+                    A p3
+                      (fun (R : Obj) (k : Hom p3 R) =>
+                       α R (α0 R (α1 R (α2 R k))))) p2 
+                   #) =>
+         f p1 (fun (R : Obj) (k : Hom p1 R) => α0 R k)
+           (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+            ᶠcons p2
+              (fun (p3 : Obj) (α2 : p2 ≤ p3) =>
+               A p3
+                 (fun (R : Obj) (k : Hom p3 R) =>
+                  α R (α0 R (α1 R (α2 R k)))))
+              (fun (p3 : Obj) (α2 : p2 ≤ p3) =>
+               a p3 (fun (R : Obj) (k : Hom p3 R) => α0 R (α1 R (α2 R k))))
+              (fun (p3 : Obj) (α2 : p2 ≤ p3) => ll p3 (α1 ∘ α2)))))
+     l0
+     (fun (p0 : Obj) (α : p ≤ p0) =>
+      P p0 (fun (R : Obj) (k : Hom p0 R) => α R k)) p 
+     #
+       ).
 
-+ apply H0.
-+ apply HS.
-- intros.
-  apply eq_sym in HeqA0; destruct HeqA0.
-  apply a.
-- intros r g.
-  apply eq_sym in HeqA0; destruct HeqA0.
-  eapply (X r g (f ∘ g)).
-reflexivity.
-Fail Defined.
-Abort.
+  (* Now the definition using a fixpoint *)
+
+  exact ((fix F p (A : Type_of_A p) (P : Type_of_P p A)
+              (Hnil : Type_of_Hnil p A P)
+              (Hcons : Type_of_Hcons p A P)
+              (l0 : ᶠlist p A p #) : Goal_Type p A P Hnil Hcons l0   
+         := 
+            match l0 with
+            | nil_ _ _ =>       Hnil p #
+            | cons_ _ _ a ll => 
+              Hcons p # a ll 
+                   (fun (p1 : Obj) (α1 : p ≤ p1) =>
+                      F p1
+                        (fun p1 f1 => A     p1 (α1 ∘ f1))
+                        (fun p1 f1 => P     p1 (α1 ∘ f1))
+                        (fun p1 f1 => Hnil  p1 (α1 ∘ f1))
+                        (fun p1 f1 => Hcons p1 (α1 ∘ f1))
+                        (ll p1 α1)) end
+          ) p A P Hnil Hcons (l p #)).
+Defined.
+
+Definition bar2 := fun A (P : list A -> Type)
+    (H0 : P (nil _))
+    (HS : forall (a:A) (l:list A), list_mem _ _ l P -> list_mem _ _ (cons _ a l) P)
+    x (l : list A) => list_rect A P H0 HS (cons A x l).
+
+Definition qux2 := fun A (P : list A -> Type)
+    (H0 : P (nil _))
+    (HS : forall (a:A) (l:list A), list_mem _ _ l P -> list_mem _ _ (cons _ a l) P)
+    x (l : list A) => HS x l (list_rect A P H0 HS l).
+
+Forcing Translate bar2 using Obj Hom.
+Forcing Translate qux2 using Obj Hom.
+
+Eval compute in ᶠbar2.
+Eval compute in ᶠqux2.
+
+Check (eq_refl : ᶠbar2 = ᶠqux2).
 
 End List.
