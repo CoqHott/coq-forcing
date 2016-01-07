629a56078b309b6f36b014930b93f13f4c38141d
nicolas.tabareau <nicolas.tabareau@inria.fr>
2016-01-07 14:56:39 +0100

    new version of nat

--- Nat.v
+++ Nat.v
@@ -9,14 +9,14 @@ Notation "P ≤ Q" := (forall R, Hom Q R -> Hom P R) (at level 70).
 Notation "#" := (fun (R : Obj) (k : Hom _ R) => k).
 Notation "f ∘ g" := (fun (R : Obj) (k : Hom _ R) => f R (g R k)) (at level 40).
 
-
-Inductive nat_ (p : Obj) : Type :=
-| O_ : nat_ p
-| S_ : (forall r (g : p ≤ r), nat_ r) -> nat_ p.
+Inductive nat_ (p : Obj) : forall q (f : p ≤ q), Type :=
+| O_ : nat_ p p #
+| S_ : (forall q (f : p ≤ q), nat_ q q #) -> nat_ p p #.
 
 Forcing Definition nat : Type using Obj Hom.
 Proof.
-exact (fun p q f => nat_ q).
+  exact nat_. 
+  (* exact (fun p q f => nat_ p q f). *)
 Defined.
 
 Forcing Definition O : nat using Obj Hom.
@@ -40,7 +40,7 @@ Fixpoint nat_rec_ (p : Obj)
   (HS : forall (p0 : Obj) (α : p ≤ p0),
        (forall (p : Obj) (α0 : p0 ≤ p), P p (α ∘ α0) p #) ->
        P p0 (fun (R : Obj) (k : Hom p0 R) => α R k) p0 #)
-  (n : nat_ p):
+  q f (n : nat_ p q f):
    P p # p # := match n with
             | O_ _ =>    H0 p #
             | S_ _ n0 => HS p #
@@ -49,14 +49,14 @@ Fixpoint nat_rec_ (p : Obj)
                         (fun p1 f1 => P  p1 (α1 ∘ f1))
                         (fun p1 f1 => H0 p1 (α1 ∘ f1))
                         (fun p1 f1 => HS p1 (α1 ∘ f1))
-                        (n0 p1 α1))
+                        _ _ (n0 p1 α1))
             end.
 
 
 Forcing Definition nat_rec : forall (P : Type), P -> (P -> P) -> nat -> P using Obj Hom.
 Proof.
   intros p P H0 HS n. 
-  exact (nat_rec_ p P H0 HS (n p #)).
+  exact (nat_rec_ p P H0 HS p # (n p #)).
 Defined.
 
 Definition foo := fun (P : Type) (H0 : P) (HS : P -> P) => nat_rec P H0 HS O.
@@ -72,6 +72,8 @@ Eval compute in ᶠqux.
 
 Check (eq_refl : ᶠbar = ᶠqux).
 
+Scheme nat_rect_ := Induction for nat_ Sort Type.
+
 Definition nat_mem : forall R, nat -> (nat -> R) -> R :=
   fun R : Type =>
     nat_rec ((nat -> R) -> R) (fun f => f O)
@@ -84,15 +86,16 @@ Forcing Definition nat_rect : forall (P : nat -> Type),
     (forall (n:nat), nat_mem _ n P -> nat_mem _ (S n) P) ->
     forall n : nat, nat_mem _ n P using Obj Hom.
 Proof.
+
   intros p P H0 HS n. unfold ᶠnat_mem, ᶠnat_rec . set (n0 := n p #). clearbody n0; clear n.
 
   (* avoiding noise in the actual definition *)
   (* may be improved using LTac ? *)
   
   set (Type_of_P := fun p => forall p0 : Obj,
-                  p ≤ p0 ->
-                  (forall p : Obj, p0 ≤ p -> ᶠnat p p #) ->
-                  forall p : Obj, p0 ≤ p -> Type).
+      p ≤ p0 ->
+      (forall p : Obj, p0 ≤ p -> ᶠnat p p #) ->
+      forall p : Obj, p0 ≤ p -> Type).
   set (Type_of_H0 := fun p (P:Type_of_P p) => forall (p0 : Obj) (α : p ≤ p0),
        P p0 (# ∘ (α ∘ #)) (fun (p : Obj) (_ : p0 ≤ p) => ᶠO p) p0 #).
   set (Type_of_HS := fun p (P:Type_of_P p) => forall (p0 : Obj) (α : p ≤ p0)
@@ -112,9 +115,9 @@ Proof.
           ᶠS p
             (fun (p1 : Obj) (α1 : p ≤ p1) => n p1 (# ∘ (α0 ∘ (α1 ∘ #)))))
          (fun (p1 : Obj) (α0 : p0 ≤ p1) =>
-          P p1 (α ∘ α0)) p0 
+          P p1 (# ∘ (# ∘ (α ∘ (# ∘ (# ∘ (α0 ∘ #))))))) p0 
          #).
-  set (Type_of_Goal := fun p (P:Type_of_P p) (H0:Type_of_H0 p P) (HS:Type_of_HS p P) n0 => nat_rec_ p
+  set (Type_of_Goal := fun p (P:Type_of_P p) (H0:Type_of_H0 p P) (HS:Type_of_HS p P) q f (n0: nat_ p q f) => nat_rec_ p
      (fun (p0 : Obj) (_ : p ≤ p0) (p1 : Obj) (_ : p0 ≤ p1) =>
       (forall p2 : Obj,
        p1 ≤ p2 ->
@@ -144,19 +147,22 @@ Proof.
            (n : forall p2 : Obj, p1 ≤ p2 -> ᶠnat p2 p2 #) =>
          f p1 (fun (R : Obj) (k : Hom p1 R) => α0 R k)
            (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
-            ᶠS p2 (fun (p3 : Obj) (α2 : p2 ≤ p3) => n p3 (α1 ∘ α2))))) n0
+            ᶠS p2 (fun (p3 : Obj) (α2 : p2 ≤ p3) => n p3 (α1 ∘ α2))))) q f n0
      (fun (p0 : Obj) (α : p ≤ p0) =>
       P p0 (fun (R : Obj) (k : Hom p0 R) => α R k)) p 
      #).
 
+  change (Type_of_Goal p P H0 HS p # n0).
+
+  revert p P H0 HS n0.
+  compute. 
   (* Now the definition using a fixpoint *)
-  
-  exact ((fix F p 
+  refine (fix F p 
               (P : Type_of_P p)
               (H0 : Type_of_H0 p P)
               (HS : Type_of_HS p P)
-              (n0 : ᶠnat p p #) : Type_of_Goal p P H0 HS n0
-             := match n0 as n1 return Type_of_Goal p P H0 HS n1 with
+              (n0 :nat_ p p #) : Type_of_Goal p P H0 HS p # n0
+             := match n0 as n1 in nat_ _ q f return Type_of_Goal p P H0 HS q f n1 with
             | O_ _ =>   H0 p #
             | S_ _ n => HS p # n
                    (fun (p1 : Obj) (α1 : p ≤ p1) =>
@@ -165,7 +171,8 @@ Proof.
                         (fun p1 f1 => H0 p1 (α1 ∘ f1))
                         (fun p1 f1 => HS p1 (α1 ∘ f1))
                         (n p1 α1)) end
-         ) p P H0 HS n0).
+         ).
+
 Defined.
 
 
