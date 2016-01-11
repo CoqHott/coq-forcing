f504f4de4ab631a9a2db86efb901762362e494f5
nicolas.tabareau <nicolas.tabareau@inria.fr>
2016-01-11 12:18:08 +0100

    cleaning of univalence

--- univalence.v
+++ univalence.v
@@ -32,14 +32,10 @@ Notation "x = y" := (x = y :>_) : type_scope.
 
 Forcing Translate eq using Obj Hom.
 
-(* Forcing Definition foo : forall A (x y:A), eq x y using Obj Hom. *)
-
 Definition pointwise_paths {A} {P:A->Type} (f g:forall x:A, P x)
   := forall x:A, eq (f x) (g x).
 
 Notation "f == g" := (pointwise_paths f g) (at level 70, no associativity) : type_scope.
-
-(* Notation "g ° f" := (fun x => g (f x)) (at level 70) : type_scope. *)
 
 Definition univalence := forall (A B : Type) (f:A -> B) (g:B -> A),
     (fun x => g (f x)) == @id _ ->
@@ -56,29 +52,14 @@ Fixpoint even n := match n with 0 => true | 1 => false | S (S n) => even n end.
 Definition A₀ := fun p q (α : p ≤ q) r (β : q ≤ r) => if r then True else False.
 Definition A₁ := fun p q (α : p ≤ q) r (β : q ≤ r) => if r then False else True. 
 
-Definition pointwise_paths_ {A} {P:A->Type} (f g:forall x:A, P x)
-  := forall x:A, f x = g x.
-
-Definition apD10_ {A} {B:A->Type}
+Definition apD10 {A} {B:A->Type}
            (f g : forall x, B x) (h: f = g) :
-    pointwise_paths_ f g :=
+    f == g :=
   match h with eq_refl _ => fun x => eq_refl _ end.
-
-Definition type_naturalityA : forall p p0
-                                       (α : p ≤ p0) p1 (α0 : p0 ≤ p1),
-    @Logic.eq Type (A₁ p p0 α p1 α0) (A₁ p p1 (α ∘ α0) p1 #).
-Proof. intros. compute. destruct p1; reflexivity.
-Defined.
-
-(* Definition type_monotonicityA : forall p p0 (α : p ≤ p0) p1 (α0 : p0 ≤ p1), *)
-(*     A₁ p p0 α p0 # -> A₁ p p1 (α ∘ α0) p1 #. *)
-(* Proof. *)
-(*   intros. destruct p1; compute in *; auto.  destruct p0; auto. *)
-(* Abort. *)
 
 Definition neg (b:bool) : bool := if b then false else true. 
 
-Axiom funext_ : forall A (B : A -> Type) (f g : forall a, B a), pointwise_paths_ f g -> f = g.
+Axiom funext_ : forall A (B : A -> Type) (f g : forall a, B a), f == g -> f = g.
 
 Definition funext := forall A (B : A -> Type) (f g : forall a, B a), f == g -> f = g.
 
@@ -102,7 +83,7 @@ Proof.
   apply eq__is_eq, funext_. intro q.
   apply funext_. intro α. apply funext_. intro a. 
   specialize (H q α a). apply eq_is_eq_ in H. 
-  apply apD10_ in H. specialize (H q). apply apD10_ in H. exact (H #).
+  apply apD10 in H. specialize (H q). apply apD10 in H. exact (H #).
 Qed.
 
 Forcing Definition neg_univ : univalence -> Empty using Obj Hom.
@@ -124,10 +105,10 @@ Proof.
   
   (* Proof of False using A₀ = A₁ *)
 
-  apply eq_is_eq_, apD10_ in huniv. specialize (huniv p).
-  apply apD10_ in huniv. specialize (huniv #).
-  apply apD10_ in huniv. specialize (huniv p). 
-  apply apD10_ in huniv. specialize (huniv #). compute in *.
+  apply eq_is_eq_, apD10 in huniv. specialize (huniv p).
+  apply apD10 in huniv. specialize (huniv #).
+  apply apD10 in huniv. specialize (huniv p). 
+  apply apD10 in huniv. specialize (huniv #). compute in *.
   destruct p. 
   exact ((fun X => match huniv in _ = X return X with eq_refl _ => I end) False).  
   symmetry in huniv.
