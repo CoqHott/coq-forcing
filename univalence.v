6be706c265ff25e24c4a9373b5456be903e3fb08
nicolas.tabareau <nicolas.tabareau@inria.fr>
2016-01-11 11:27:40 +0100

    univalent using eq

--- univalence.v
+++ univalence.v
@@ -13,13 +13,6 @@ Notation "P ≤ Q" := (forall R, Hom Q R -> Hom P R) (at level 70).
 Notation "#" := (fun (R : Obj) (k : Hom _ R) => k).
 Notation "f ∘ g" := (fun (R : Obj) (k : Hom _ R) => f R (g R k)) (at level 40).
 
-Forcing Definition eq : forall (A : Type), A -> A -> Type using Obj Hom.
-Proof.
-compute in *.
-intros p A x y q α.
-exact (x = y).
-Defined.
-
 Forcing Definition Empty : Type using Obj Hom.
 Proof.
 intros p q α.
@@ -32,17 +25,26 @@ intros p A e.
 destruct (e p #).
 Defined.
 
+Inductive eq {A : Type} (x : A) : A -> Type :=  eq_refl : eq x x
+where "x = y :> A" := (@eq A x y) : type_scope.
+
+Notation "x = y" := (x = y :>_) : type_scope.
+
+Forcing Translate eq using Obj Hom.
+
+(* Forcing Definition foo : forall A (x y:A), eq x y using Obj Hom. *)
+
 Definition pointwise_paths {A} {P:A->Type} (f g:forall x:A, P x)
-  := forall x:A, eq _ (f x) (g x).
+  := forall x:A, eq (f x) (g x).
 
 Notation "f == g" := (pointwise_paths f g) (at level 70, no associativity) : type_scope.
 
-Notation "g ° f" := (fun x => g (f x)) (at level 70) : type_scope.
+(* Notation "g ° f" := (fun x => g (f x)) (at level 70) : type_scope. *)
 
 Definition univalence := forall (A B : Type) (f:A -> B) (g:B -> A),
-    g ° f == @id _ ->
-    f ° g == @id _ ->
-    eq Type A B.
+    (fun x => g (f x)) == @id _ ->
+    (fun x => f (g x)) == @id _ ->
+    A = B.
 
 Forcing Translate pointwise_paths using Obj Hom.
 Forcing Translate ID using Obj Hom.
@@ -57,8 +59,10 @@ Definition A₁ := fun p q (α : p ≤ q) r (β : q ≤ r) => if r then False el
 Definition pointwise_paths_ {A} {P:A->Type} (f g:forall x:A, P x)
   := forall x:A, f x = g x.
 
-Definition apD10_ {A} {B:A->Type} {f g : forall x, B x} (h: f = g) :pointwise_paths_ f g :=
-  match h with eq_refl => fun x => eq_refl end. 
+Definition apD10_ {A} {B:A->Type}
+           (f g : forall x, B x) (h: f = g) :
+    pointwise_paths_ f g :=
+  match h with eq_refl _ => fun x => eq_refl _ end.
 
 Definition type_naturalityA : forall p p0
                                        (α : p ≤ p0) p1 (α0 : p0 ≤ p1),
@@ -66,25 +70,38 @@ Definition type_naturalityA : forall p p0
 Proof. intros. compute. destruct p1; reflexivity.
 Defined.
 
-Definition type_monotonicityA : forall p p0 (α : p ≤ p0) p1 (α0 : p0 ≤ p1),
-    A₁ p p0 α p0 # -> A₁ p p1 (α ∘ α0) p1 #.
-Proof.
-  intros. destruct p1; compute in *; auto.  destruct p0; auto.
-Abort.
+(* Definition type_monotonicityA : forall p p0 (α : p ≤ p0) p1 (α0 : p0 ≤ p1), *)
+(*     A₁ p p0 α p0 # -> A₁ p p1 (α ∘ α0) p1 #. *)
+(* Proof. *)
+(*   intros. destruct p1; compute in *; auto.  destruct p0; auto. *)
+(* Abort. *)
 
 Definition neg (b:bool) : bool := if b then false else true. 
 
 Axiom funext_ : forall A (B : A -> Type) (f g : forall a, B a), pointwise_paths_ f g -> f = g.
 
-Definition funext := forall A (B : A -> Type) (f g : forall a, B a), f == g -> eq _ f g.
+Definition funext := forall A (B : A -> Type) (f g : forall a, B a), f == g -> f = g.
 
 Forcing Translate funext using Obj Hom.
 
+Definition eq__is_eq : forall p A (x y: forall p0 (f:p ≤ p0), A p0 f p0 #),
+    x = y -> ᶠeq p _ x y p #.
+Proof.
+  intros. destruct H. apply ᶠeq_refl.
+Defined.
+
+Definition eq_is_eq_ : forall p A (x y: forall p0 (f:p ≤ p0), A p0 f p0 #),
+   ᶠeq p _ x y p # -> x = y.
+Proof.
+  intros. destruct H. apply eq_refl.
+Defined. 
+
 Forcing Definition funext_preservation : funext using Obj Hom.
 Proof.
-  intros p A B f g H. compute in *.
-  apply funext_. intro q. apply funext_. intro α. apply funext_. intro a. 
-  specialize (H q α a).
+  intros p A B f g H. 
+  apply eq__is_eq, funext_. intro q.
+  apply funext_. intro α. apply funext_. intro a. 
+  specialize (H q α a). apply eq_is_eq_ in H. 
   apply apD10_ in H. specialize (H q). apply apD10_ in H. exact (H #).
 Qed.
 
@@ -107,99 +124,28 @@ Proof.
   
   (* Proof of False using A₀ = A₁ *)
 
-  apply apD10_ in huniv. specialize (huniv p).
+  apply eq_is_eq_, apD10_ in huniv. specialize (huniv p).
   apply apD10_ in huniv. specialize (huniv #).
   apply apD10_ in huniv. specialize (huniv p). 
   apply apD10_ in huniv. specialize (huniv #). compute in *.
   destruct p. 
-  exact ((fun X => match huniv in _ = X return X with eq_refl => I end) False).  
+  exact ((fun X => match huniv in _ = X return X with eq_refl _ => I end) False).  
   symmetry in huniv.
-  exact ((fun X => match huniv in _ = X return X with eq_refl => I end) False).  
+  exact ((fun X => match huniv in _ = X return X with eq_refl _ => I end) False).  
 
   (* Dealing with section and retraction *)
   
-  compute. intros. apply funext_. intro p1. apply funext_. intro α0.
-  assert ((fun (R : bool) (_ : Hom true R) => α0 R tt) = α0).
+  compute. intros. apply eq__is_eq, funext_. intro p1. apply funext_. intro α0.
+  assert ((fun (R : bool) (_ : Hom true R) => α0 R tt) = (fun (R : bool) (k : Hom p1 R) => α0 R k)).
   apply funext_. intro p2. apply funext_. intro α1. destruct α1. reflexivity.
-  destruct H. destruct p1; reflexivity.
+  rewrite <- H. destruct p1; reflexivity.
 
-  compute. intros. apply funext_. intro p1. apply funext_. intro α0.
-  assert ((fun (R : bool) (_ : Hom true R) => α0 R tt) = α0).
+  compute. intros. apply eq__is_eq, funext_. intro p1. apply funext_. intro α0.
+  assert ((fun (R : bool) (_ : Hom true R) => α0 R tt) = (fun (R : bool) (k : Hom p1 R) => α0 R k)).
   apply funext_. intro p2. apply funext_. intro α1. destruct α1. reflexivity.
-  destruct H. destruct p1; reflexivity.
+  rewrite <- H. destruct p1; reflexivity.
   
 Defined.
-
-
-Axiom propext_ : forall (P Q : Type), (P -> Q) -> (Q -> P) -> P = Q. 
-
-Definition propext_inv : forall (P Q : Type), P = Q -> P -> Q.
-Proof. intros. destruct H. exact X. Defined.
-
-Axiom type_naturality@{i j} : forall p
-                (A : forall p0 (α : p ≤ p0) p1 (α0 : p0 ≤ p1), Type@{i}) 
-                p0 (α : p ≤ p0) p1 (α0 : p0 ≤ p1),
-    @Logic.eq Type@{j} (A p0 α p1 α0) (A p1 (α ∘ α0) p1 #).
-
-Forcing Definition Box : Type -> Type using Obj Hom.
-Proof.
-  intros. exact (forall p1 (α0 : p0 ≤ p1), X p1 (α ∘ α0) p1 #). 
-Defined.
-
-Forcing Definition Box_unit : forall (A:Type) , A -> Box A using Obj Hom.
-Proof.
-  intros p A. compute. exact (@id _). 
-Defined.   
-
-Forcing Definition Box_counit : forall (A:Type) , Box A -> A using Obj Hom.
-Proof.
-  intros. exact (X p # p #).
-Defined. 
-
-Forcing Definition Box_unit_counit : forall (A:Type) x, eq _ (Box_counit A (Box_unit A x)) x using Obj Hom.
-Proof.
-  intros. reflexivity.
-Defined.
-
-Axiom box_naturality@{i} : forall p
-                (A : forall p0 (α : p ≤ p0) p1 (α0 : p0 ≤ p1), Type@{i})
-                (x : forall p0 (α : p ≤ p0) p1 (α0 : p0 ≤ p1), A p1 (α ∘ α0) p1 #) p0
-                (α : p ≤ p0) p1 (α0 : p0 ≤ p1),
-    @Logic.eq (A p1 (α ∘ α0) p1 #) (x p0 α p1 α0) (x p1 (α ∘ α0) p1 #).
-
-Forcing Definition Box_modality_fun : forall (A B:Type), (A -> Box B) -> Box A -> Box B using Obj Hom.
-Proof.
-  intros p A B f x.
-  compute in *. intros. apply f. intros. apply x.
-Defined.
-
-Definition Box_modality_inv : forall (A B:Type), (Box A -> Box B) -> A -> Box B :=
-  fun A B f x => f (Box_unit _ x).  
-                                                  
-Forcing Translate Box_modality_inv using Obj Hom.
-
-
-Forcing Definition Box_modality : forall (A B:Type), (A -> Box B) -> Box A -> Box B using Obj Hom.
-Proof.
-  intros p A B f x. 
-  compute in *. intros. apply f. intros. apply x.
-Defined.
-
-Definition propext_Box := forall (A B : Type), (A -> B) -> (B -> A) ->
-                                               eq Type (Box A) (Box B).
-
-Forcing Translate propext_Box using Obj Hom.
-
-Forcing Definition propext_preservation_Box : propext_Box using Obj Hom.
-Proof.
-  intros p P Q H H'. compute in *. 
-  apply funext_. intro q. apply funext_. intro α. apply funext_. intro r. 
-  apply funext_. intro α1. apply propext_. 
-  - intro HP. intros. rewrite type_naturality.
-    apply H. intros. change (P _ (α ∘ α1 ∘ α0∘ α2) _ #). apply HP. 
-  - intro HQ. intros. rewrite type_naturality.
-    apply H'. intros. change (Q _ (α ∘ α1 ∘ α0∘ α2) _ #). apply HQ. 
-Defined. 
 
   
 End Univalence. 
