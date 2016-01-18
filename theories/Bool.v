53d768f6d543bd0db130aaf70148004c864e8756
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2016-01-18 17:03:48 +0100

    Fixing Bool.

--- theories/Bool.v
+++ theories/Bool.v
@@ -9,24 +9,26 @@ Notation "P ≤ Q" := (forall R, Hom Q R -> Hom P R) (at level 70).
 Notation "#" := (fun (R : Obj) (k : Hom _ R) => k).
 Notation "f ∘ g" := (fun (R : Obj) (k : Hom _ R) => f R (g R k)) (at level 40).
 
-Inductive ᶠᶠbool p : forall q, p ≤ q -> Type :=
-| ᶠᶠtrue : ᶠᶠbool p p #
-| ᶠᶠfalse : ᶠᶠbool p p #.
+Forcing Translate bool using Obj Hom.
 
-Forcing Definition bool : Type using Obj Hom.
-Admitted.
-
-Forcing Definition true : bool using Obj Hom.
-Admitted.
-
-Forcing Definition false : bool using Obj Hom.
-Admitted.
-
-Forcing Definition bool_rect : forall P,
-  P true -> P false -> forall b, P b using Obj Hom.
+Forcing Definition bool_rec : forall P, P -> P -> bool -> P using Obj Hom.
 Proof.
 intros p P Ptt Pff b.
-change (P p # b p #).
+destruct (b p #); [apply Ptt|apply Pff].
+Defined.
+
+Definition bool_mem : forall R, bool -> (bool -> R) -> R :=
+  fun R b => bool_rec ((bool -> R) -> R) (fun k => k true) (fun k => k false) b.
+
+Forcing Translate bool_mem using Obj Hom.
+
+Forcing Definition bool_rect : forall P,
+  P true -> P false -> forall b, bool_mem _ b P using Obj Hom.
+Proof.
+intros p P Ptt Pff b.
+compute.
+destruct (b p #); [apply Ptt|apply Pff].
+Fail Defined. (** stupid universe errors *)
 Abort.
 
 End Bool.
