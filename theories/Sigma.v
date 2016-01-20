9c403686a55a4f13a03d27cd62a8027cd7fc2f55
nicolas.tabareau <nicolas.tabareau@inria.fr>
2016-01-20 15:42:24 +0100

    positive sigma

--- theories/Sigma.v
+++ theories/Sigma.v
@@ -11,47 +11,144 @@ Notation "P ≤ Q" := (forall R, Hom Q R -> Hom P R) (at level 70).
 Notation "#" := (fun (R : Obj) (k : Hom _ R) => k).
 Notation "f ∘ g" := (fun (R : Obj) (k : Hom _ R) => f R (g R k)) (at level 40).
 
-Record _sig_ p
-  (A : forall p0 : Obj, p ≤ p0 -> forall p : Obj, p0 ≤ p -> Type)
-  (P : forall (p0 : Obj) (α : p ≤ p0),
-    (forall (p1 : Obj) (α0 : p0 ≤ p1), A p1 (# ∘ (α ∘ (# ∘ (α0 ∘ #)))) p1 #) -> forall p : Obj, p0 ≤ p -> Type)
-  p0 (α : p ≤ p0) : Type := _exist_
-{
-  _proj1_ : (forall p1 (α0 : p0 ≤ p1), A p1 (α ∘ α0) p1 #);
-  _proj2_ : (forall p1 (α0 : p0 ≤ p1), P p1 (α ∘ α0) (fun p2 α1 => _proj1_ p2 (α0 ∘ α1)) p1 #);
-}.
+Forcing Translate sigT using Obj Hom.
 
-Forcing Definition sig : forall (A : Type) (P : A -> Type), Type using Obj Hom.
-Proof.
-exact _sig_.
-Defined.
+Definition sig_rec : forall A (B:A -> Type) P, (forall a:A, B a -> P) -> sigT B -> P :=
+  fun A B P f x => match x with existT _ a b => f a b end.
 
-Forcing Definition exist : forall A (P : A -> Type) (x : A) (p : P x), sig A P using Obj Hom.
-Proof.
-intros p A P x π.
-refine (_exist_ _ _ _ _ _ _ π).
-Defined.
+Forcing Translate sig_rec using Obj Hom.
 
-Forcing Definition proj1 : forall A P, sig A P -> A using Obj Hom.
-Proof.
-intros p A P σ.
-refine ((σ p #).(_proj1_ p A _ p #) p #).
-Defined.
+Definition sig_mem A (B:A -> Type) : forall R, sigT B -> (sigT B -> R) -> R:=
+  fun R x => sig_rec A B (({x : A & B x} -> R) -> R) (fun a b k => k (existT _ a b)) x.
 
-(*
-Forcing Definition proj2 : forall A P (σ : sig A P), P (proj1 A P σ) using Obj Hom.
-Proof.
-intros p A P σ.
-compute.
-change (
-P p #
-  (fun (p0 : Obj) (α : p ≤ p0) => (σ p0 α).(_proj1_ _ _ _ _ _) p0 #) p 
-  #).
-eapply ((σ p #).(_proj2_ _ _ _ _ _) p #).
+Forcing Translate sig_mem using Obj Hom.
 
-eapply _proj2_.
-
+Forcing Definition sig_rect' : forall A (B:A -> Type) P,
+    (forall (a:A) (b: B a), P (existT _ a b)) -> forall (x : sigT B), sig_mem A B _ x P
+                                                     using Obj Hom.
+intros p A B P H x.
+compute. generalize (x p #).
+exact (fun x => match x with
+        | existTᶠ _ _ _ a b => H p # a b
+                end).
+(* Universe issue *)
+Show Proof.
 Abort.
-*)
+
+Forcing Definition sig_rect' : forall A (B:A -> Type) P,
+    (forall (a:A) (b: B a), P (existT _ a b)) -> forall (x : sigT B), sig_mem A B _ x P
+                                                     using Obj Hom.
+
+exact (fun (p : Obj)
+   (A : forall p0 : Obj, p ≤ p0 -> forall p1 : Obj, p0 ≤ p1 -> Type)
+   (B : forall (p0 : Obj) (α : p ≤ p0),
+        (forall (p1 : Obj) (α0 : p0 ≤ p1),
+         A p1 (# ∘ (α ∘ (# ∘ (α0 ∘ #)))) p1 #) ->
+        forall p1 : Obj, p0 ≤ p1 -> Type)
+   (P : forall (p0 : Obj) (α : p ≤ p0),
+        (forall (p1 : Obj) (α0 : p0 ≤ p1),
+         (fun
+            (A0 : forall p2 : Obj,
+                  p1 ≤ p2 -> forall p3 : Obj, p2 ≤ p3 -> Type)
+            (P : forall (p2 : Obj) (α1 : p1 ≤ p2),
+                 (forall (p3 : Obj) (α2 : p2 ≤ p3),
+                  A0 p3 (α1 ∘ (# ∘ (α2 ∘ #))) p3 #) ->
+                 forall p3 : Obj, p2 ≤ p3 -> Type) 
+            (p2 : Obj) (α1 : p1 ≤ p2) =>
+          sigTᶠ p2
+            (fun (p3 : Obj) (α2 : p2 ≤ p3) => A0 p3 (α1 ∘ (α2 ∘ #)))
+            (fun (p3 : Obj) (α2 : p2 ≤ p3) => P p3 (α1 ∘ (α2 ∘ #))))
+           (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+            A p2 (# ∘ (# ∘ (α ∘ (# ∘ (α0 ∘ (α1 ∘ #)))))))
+           (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+            B p2 (# ∘ (α ∘ (# ∘ (α0 ∘ (α1 ∘ #)))))) p1 
+           #) -> forall p1 : Obj, p0 ≤ p1 -> Type)
+   (H : forall (p0 : Obj) (α : p ≤ p0)
+          (a : forall (p1 : Obj) (α0 : p0 ≤ p1),
+               A p1 (# ∘ (# ∘ (# ∘ (α ∘ (# ∘ (α0 ∘ #)))))) p1 #)
+          (b : forall (p1 : Obj) (α0 : p0 ≤ p1),
+               B p1 (# ∘ (# ∘ (α ∘ (# ∘ (# ∘ (α0 ∘ #))))))
+                 (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+                  a p2 (# ∘ (α0 ∘ (α1 ∘ #)))) p1 
+                 #),
+        P p0 (# ∘ (α ∘ (# ∘ (# ∘ #))))
+          (fun (p1 : Obj) (α0 : p0 ≤ p1) =>
+           existTᶠ p1
+             (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+              A p2 (# ∘ (# ∘ (# ∘ (α ∘ (# ∘ (# ∘ (α0 ∘ (α1 ∘ #)))))))))
+             (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+              B p2 (# ∘ (# ∘ (α ∘ (# ∘ (# ∘ (α0 ∘ (α1 ∘ #))))))))
+             (fun (p2 : Obj) (α1 : p1 ≤ p2) => a p2 (# ∘ (α0 ∘ (α1 ∘ #))))
+             (fun (p2 : Obj) (α1 : p1 ≤ p2) => b p2 (α0 ∘ (α1 ∘ #)))) p0 
+          #)
+   (x : forall (p0 : Obj) (α : p ≤ p0),
+        (fun
+           (A0 : forall p1 : Obj,
+                 p0 ≤ p1 -> forall p2 : Obj, p1 ≤ p2 -> Type)
+           (P0 : forall (p1 : Obj) (α0 : p0 ≤ p1),
+                 (forall (p2 : Obj) (α1 : p1 ≤ p2),
+                  A0 p2 (α0 ∘ (# ∘ (α1 ∘ #))) p2 #) ->
+                 forall p2 : Obj, p1 ≤ p2 -> Type) 
+           (p1 : Obj) (α0 : p0 ≤ p1) =>
+         sigTᶠ p1 (fun (p2 : Obj) (α1 : p1 ≤ p2) => A0 p2 (α0 ∘ (α1 ∘ #)))
+           (fun (p2 : Obj) (α1 : p1 ≤ p2) => P0 p2 (α0 ∘ (α1 ∘ #))))
+          (fun (p1 : Obj) (α0 : p0 ≤ p1) =>
+           A p1 (# ∘ (# ∘ (# ∘ (# ∘ (α ∘ (α0 ∘ #)))))))
+          (fun (p1 : Obj) (α0 : p0 ≤ p1) =>
+           B p1 (# ∘ (# ∘ (# ∘ (α ∘ (α0 ∘ #)))))) p0 
+          #) =>
+ (fun
+    x0 : sigTᶠ p
+           (fun (p0 : Obj) (α : p ≤ p0) =>
+            A p0 (fun (R : Obj) (k : Hom p0 R) => α R k))
+           (fun (p0 : Obj) (α : p ≤ p0) =>
+            B p0 (fun (R : Obj) (k : Hom p0 R) => α R k)) =>
+  match
+    x0 as x1
+    return
+      (match x1 with
+       | existTᶠ _ _ _ a b =>
+           fun
+             k : forall (p0 : Obj) (α : p ≤ p0),
+                 (forall (p1 : Obj) (α0 : p0 ≤ p1),
+                  sigTᶠ p1
+                    (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+                     A p2
+                       (fun (R : Obj) (k : Hom p2 R) =>
+                        α R (α0 R (α1 R k))))
+                    (fun (p2 : Obj) (α1 : p1 ≤ p2)
+                       (x2 : forall (p3 : Obj) (α2 : p2 ≤ p3),
+                             A p3
+                               (fun (R : Obj) (k : Hom p3 R) =>
+                                α R (α0 R (α1 R (α2 R k)))) p3 
+                               #) =>
+                     B p2
+                       (fun (R : Obj) (k : Hom p2 R) =>
+                        α R (α0 R (α1 R k)))
+                       (fun (p3 : Obj) (α2 : p2 ≤ p3) =>
+                        x2 p3 (fun (R : Obj) (k : Hom p3 R) => α2 R k)))) ->
+                 forall p1 : Obj, p0 ≤ p1 -> Type =>
+           k p #
+             (fun (p0 : Obj) (α : p ≤ p0) =>
+              existTᶠ p0 (fun (p1 : Obj) (α0 : p0 ≤ p1) => A p1 (α ∘ α0))
+                (fun (p1 : Obj) (α0 : p0 ≤ p1)
+                   (x2 : forall (p2 : Obj) (α1 : p1 ≤ p2),
+                         A p2
+                           (fun (R : Obj) (k0 : Hom p2 R) =>
+                            α R (α0 R (α1 R k0))) p2 
+                           #) =>
+                 B p1 (α ∘ α0)
+                   (fun (p2 : Obj) (α1 : p1 ≤ p2) =>
+                    x2 p2 (fun (R : Obj) (k0 : Hom p2 R) => α1 R k0)))
+                (fun (p1 : Obj) (α0 : p0 ≤ p1) => a p1 (α ∘ α0))
+                (fun (p1 : Obj) (α0 : p0 ≤ p1) => b p1 (α ∘ α0)))
+       end
+         (fun (p0 : Obj) (α : p ≤ p0) =>
+          P p0 (fun (R : Obj) (k : Hom p0 R) => α R k)) p 
+         #)
+  with
+  | existTᶠ _ _ _ a b => H p # a b
+  end) (x p #)).
+Defined. 
 
 End Sigma.
